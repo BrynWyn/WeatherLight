c7d6af1b5e19b01cbf8c67b4dc9bbde00df45f20
BrynWyn <ieuan.rowlands@gmail.com>
2020-04-29 18:58:08 +0100

    configuration

--- LightController.py
+++ LightController.py
@@ -1,4 +1,4 @@
-#from blinkt import set_pixel, set_brightness, show, clear
+from blinkt import set_pixel, set_brightness, show, clear
 from collections import OrderedDict
 import itertools
 
@@ -6,9 +6,9 @@ def SetupLights(rainyDays):
     #clear()
     SetupMorningLights(rainyDays[0])
     SetupEveningLights(rainyDays[1])
-    #set_brightness(0.1)
+    set_brightness(0.1)
     #set_pixel(0, 255, 255, 255)
-    #show()
+    show()
 
 def SortDays(rainyDays):
     ordered =  OrderedDict(sorted(rainyDays.items()))
@@ -18,18 +18,18 @@ def SetupMorningLights(morningDays):
     days = SortDays(morningDays)
     for i in range(len(days)):
         if(list(days.values())[i] == '1'):
-            #set_pixel(i+1, 0, 255, 0)
+            set_pixel(i*2, 255, 0, 0)
             print("Setting Pixel " + str(i*2) + " With Rain")
         else:
-            #set_pixel(i+1, 0, 0, 255)
+            set_pixel(i*2, 0, 0, 255)
             print("Setting Pixel " + str(i*2) + " Without Rain")
 
 def SetupEveningLights(eveningDays):
     days = SortDays(eveningDays)
     for i in range(len(days)):
         if(list(days.values())[i] == '1'):
-            #set_pixel(i+1, 0, 255, 0)
+            set_pixel((i*2)+1, 255, 0, 0)
             print("Setting Pixel " + str(i*2+1) + " With Rain")
         else:
-            #set_pixel(i+1, 0, 0, 255)
+            set_pixel((i*2)+1, 0, 0, 255)
             print("Setting Pixel " + str(i*2+1) + " Without Rain")
