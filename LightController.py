c3e44a6bd7c38cb9d8115434f2491d78c40950ad
BrynWyn <ieuan.rowlands@gmail.com>
2020-04-29 14:29:51 +0100

    Cutting the list to 4 day forcast

--- LightController.py
+++ LightController.py
@@ -1,5 +1,6 @@
-from blinkt import set_pixel, set_brightness, show, clear
+#from blinkt import set_pixel, set_brightness, show, clear
 from collections import OrderedDict
+import itertools
 
 def SetupLights(rainyDays):
     #clear()
@@ -7,27 +8,28 @@ def SetupLights(rainyDays):
     SetupEveningLights(rainyDays[1])
     #set_brightness(0.1)
     #set_pixel(0, 255, 255, 255)
-    show()
+    #show()
 
 def SortDays(rainyDays):
-    return OrderedDict(sorted(rainyDays.items()))
+    ordered =  OrderedDict(sorted(rainyDays.items()))
+    return dict(itertools.islice(ordered.items(), 4))
 
 def SetupMorningLights(morningDays):
     days = SortDays(morningDays)
     for i in range(len(days)):
-        if(list(days.values())[i] == 1):
-            set_pixel(i+1, 0, 255, 0)
+        if(list(days.values())[i] == '1'):
+            #set_pixel(i+1, 0, 255, 0)
             print("Setting Pixel " + str(i*2) + " With Rain")
         else:
-            set_pixel(i+1, 0, 0, 255)
+            #set_pixel(i+1, 0, 0, 255)
             print("Setting Pixel " + str(i*2) + " Without Rain")
 
 def SetupEveningLights(eveningDays):
     days = SortDays(eveningDays)
     for i in range(len(days)):
-        if(list(days.values())[i] == 1):
-            set_pixel(i+1, 0, 255, 0)
+        if(list(days.values())[i] == '1'):
+            #set_pixel(i+1, 0, 255, 0)
             print("Setting Pixel " + str(i*2+1) + " With Rain")
         else:
-            set_pixel(i+1, 0, 0, 255)
+            #set_pixel(i+1, 0, 0, 255)
             print("Setting Pixel " + str(i*2+1) + " Without Rain")
