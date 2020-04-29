5522678406f332a2043708d57329285c5e40e9dc
BrynWyn <ieuan.rowlands@gmail.com>
2020-04-29 13:07:54 +0100

    Adding in Blinkt commands

--- LightController.py
+++ LightController.py
@@ -1,7 +1,11 @@
+from blinkt import set_pixel, set_brightness, show, clear
 from collections import OrderedDict
 
 def SetupLights(rainyDays):
     a = SortDays(rainyDays)
+    set_brightness(0.1)
+    set_pixel(0, 255, 255, 255)
+    show()
     print(a)
 
 def SortDays(rainyDays):
